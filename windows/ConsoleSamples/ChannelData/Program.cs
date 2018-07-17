529a5c1e1b9781d28f3b4fcade182d6ea1596384
Daniel Vlasenko <danny.vlasenko@gmail.com>
2018-07-17 19:27:52 +0300

    win c#. channels sample

---
+++ windows/ConsoleSamples/ChannelData/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Dynamic;
+using System.Resources;
+using Neuro;
+
+namespace ChannelData
+{
+    class Program
+    {
+        private static string DeviceName { get; set; }
+        private static string DeviceAddress { get; set; }
+        private static int BatteryLevel { get; set; }
+        private static DeviceState ConnectionState { get; set; }
+        private static SamplingFrequency Frequency { get; set; }
+        private static double Duration { get; set; }
+
+        static void Main(string[] args)
+        {
+            DeviceScanner scanner = new DeviceScanner();
+            scanner.DeviceFound += Scanner_DeviceFound;
+            scanner.StartScan();
+            Console.ReadLine();
+        }
+
+        private static void Scanner_DeviceFound(object sender, Device device)
+        {
+            (sender as DeviceScanner)?.StopScan();
+            device.ParameterChanged += Device_ParameterChanged;
+            ConnectionState = device.ReadParam<DeviceState>(Parameter.State);
+            if (ConnectionState != DeviceState.Connected)
+            {
+                device.Connect();
+            }
+            else
+            {
+                OnDeviceConnected(device);
+            }
+        }
+
+        private static void OnDeviceConnected(Device device)
+        {
+            DeviceName = device.ReadParam<string>(Parameter.Name);
+            DeviceAddress = device.ReadParam<string>(Parameter.Address);
+            Frequency = device.ReadParam<SamplingFrequency>(Parameter.SamplingFrequency);
+            Rewrite();
+            var batteryChannel = new BatteryChannel(device);
+            batteryChannel.LengthChanged += BatteryChannel_LengthChanged;
+            if (Frequency != SamplingFrequency.Hz125)
+            {
+                device.SetParam(Parameter.SamplingFrequency, SamplingFrequency.Hz250);
+            }
+            var signalChannel = new SignalChannel(device, new []{Filter.LowPass_30Hz_SF250, Filter.HighPass_2Hz_SF250});
+            signalChannel.LengthChanged += SignalChannel_LengthChanged;
+            device.Execute(Command.StartSignal);
+        }
+
+        private static void SignalChannel_LengthChanged(object sender, int length)
+        {
+            Duration = (double) (length) / 250;
+            Rewrite();
+        }
+
+        private static void BatteryChannel_LengthChanged(object sender, int length)
+        {
+            BatteryLevel = (sender as BatteryChannel)?.ReadData(length - 1, 1)[0] ?? 0;
+            Rewrite();
+        }
+
+        private static void Device_ParameterChanged(object sender, Parameter parameter)
+        {
+            if (parameter != Parameter.State)
+                return;
+
+            if (!(sender is Device))
+                return;
+
+            var device = (Device) sender;
+            ConnectionState = device.ReadParam<DeviceState>(Parameter.State);
+            if (ConnectionState == DeviceState.Connected)
+            {
+                OnDeviceConnected(device);
+            }
+            else
+            {
+                Rewrite();
+            }
+        }
+
+        private static void Rewrite()
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine($"{DeviceName} [{DeviceAddress}] State: {ConnectionState} Battery: {BatteryLevel} Frequency: {Frequency} Duration: {Duration}");
+        }
+    }
+}
--- windows/ConsoleSamples/DeviceInfo/Program.cs
+++ windows/ConsoleSamples/DeviceInfo/Program.cs
@@ -11,7 +11,32 @@ namespace DeviceInfo
             scanner.DeviceFound += Scanner_DeviceFound;
             scanner.ScanStateChanged += Scanner_ScanStateChanged;
             scanner.StartScan();
-            while (Console.ReadLine() != "\n");
+            Console.ReadLine();
+        }
+
+        private static void OnDeviceConnected(Device device)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Device can execute:");
+            foreach(var cmd in device.Commands)
+            {
+                Console.WriteLine($"    -{cmd.ToString()}");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Device has parameters:");
+            foreach (var paraminfo in device.Parameters)
+            {
+                Console.WriteLine($"    -{paraminfo.Parameter} {{{paraminfo.Access}}}");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Device has channels:");
+            foreach (var channel in device.Channels)
+            {
+                Console.WriteLine($"    -{channel.Name}");
+            }
+            Console.WriteLine();
         }
 
         private static void Scanner_ScanStateChanged(object sender, bool isScanning)
@@ -21,17 +46,37 @@ namespace DeviceInfo
 
         private static void Scanner_DeviceFound(object sender, Device device)
         {
-            Console.WriteLine("Device found!");
+            var deviceName = device.ReadParam<string>(Parameter.Name);
+            var deviceAddress = device.ReadParam<string>(Parameter.Address);
+            var deviceState = device.ReadParam<DeviceState>(Parameter.State);
+
+            Console.WriteLine($"Found device {deviceName} [{deviceAddress}], state: {deviceState}");
+
             device.ParameterChanged += Device_ParameterChanged;
-            device.Connect();
+            var state = device.ReadParam<DeviceState>(Parameter.State);
+            if (state == DeviceState.Connected)
+            {
+                OnDeviceConnected(device);
+            }
+            else
+            {
+                device.Connect();
+            }
         }
 
         private static void Device_ParameterChanged(object sender, Parameter parameter)
         {
             try
             {
-                var device = (Device) sender;
-                device.Execute(Command.FindMe);
+                if (parameter == Parameter.State)
+                {
+                    var device = (Device)sender;
+                    var state = device.ReadParam<DeviceState>(Parameter.State);
+                    if (state == DeviceState.Connected)
+                    {
+                        OnDeviceConnected(device);
+                    }
+                }
             }
             catch (Exception e)
             {
